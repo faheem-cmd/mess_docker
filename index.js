25660375a6bd830a70858f3329f999a91894a161
Muhammed <faheem@metrictreelabs.com>
2022-07-28 16:09:46 +0530

    changes

--- index.js
+++ index.js
@@ -5,21 +5,9 @@ require("dotenv").config();
 const router = require("./routes/user.router");
 const { success, error } = require("consola");
 app.use("/", router);
-//MongoDB connection
-// const dbName = "mess";
-// const dbUrl = process.env.DB_URL || `mongodb://0.0.0.0:27017/${dbName}`;
 const dbUrl = process.env.DB_URL;
 
 mongoose(dbUrl);
-// mongoose.connect("mongodb://localhost/mess", { useNewUrlParser: true });
-// mongoose.connection
-//   .once("open", function () {
-//     console.log("Database connected Successfully");
-//   })
-//   .on("error", function (err) {
-//     console.log("Error", err);
-//   });
-//Server
 app.listen(process.env.PORT, () => {
   success({
     message: `Successfully connected with the database \n${dbUrl}`,
